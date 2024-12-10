30ba008fad2936a81d2489b3e14bd786c91ee0cb
alegorix <51210510+alegorix@users.noreply.github.com>
2024-12-10 10:49:16 +0100

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,1 +1,9 @@
-
+<?php
+try{
+    // Connexion à la base de données du resto (db_resto)
+    $db = new PDO('mysql:host=localhost;dbname=db_resto', 'root', '');
+    $db->exec('SET NAMES "UTF8"');
+} catch (PDOException $e){
+    echo 'Erreur : '. $e->getMessage();
+    die();
+}
