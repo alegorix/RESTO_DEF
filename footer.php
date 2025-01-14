b53ff0ab130d3f0f0166b9417dd8974a457475e0
alegorix <51210510+alegorix@users.noreply.github.com>
2025-01-14 09:06:21 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6"> 
+                <h3>À propos de nous</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A eaque aperiam numquam dignissimos consequatur quae est dolorem iure inventore fugit nostrum hic itaque eos recusandae, laboriosam aut ipsum fugiat deserunt.</p>
+            </div>
+<!--Mon commentaire html-->
+            <div class="col-md-3">
+                <h3>Adresse</h3>
+                <p>Square des Martyrs 1 <br>6000 Charleroi<br>Tél.: <a href="tel:+3271123456">071/123456</a></p>
+            </div>
+            <div class="col-md-3">
+                <h3>Horaires</h3>
+                <p>Du lundi au samedi de 10h à 22h</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</footer>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,21 @@
+<nav class="navbar fixed-top bg-dark navbar-expand-sm border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Mon restaurant</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-house"></i> Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-utensils"></i> Carte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#"><i class="fa-solid fa-envelope"></i> Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
